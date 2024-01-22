eb9713ebfa15246b89e1c9aa6ca14f48d0f72a02
Tommi <tommi.savolainen79@gmail.com>
2024-01-22 13:48:02 +0200

    lähtö ja määränpää funktiota väsätty

--- js/skriptit-ts.js
+++ js/skriptit-ts.js
@@ -1,6 +1,19 @@
 function etsiLahtoJaMaaranpaa(indeksi) {
     junat.forEach((element, index) => {
-        console.log(element);
+        if (indeksi == index) {
+            console.log(element.akt);
+            if (element.akt != null) {
+                // if (element.tiedot == null) {
+                //     element.tiedot = '';
+                //     element.tiedot.lahtopaikka = ''
+                //     element.tiedot.maaranpaa = ''
+                // }
+                element.tiedot.lahtopaikka = etsiAsemanNimi(element.akt.timeTableRows[0].stationUICCode);
+                element.tiedot.maaranpaa = etsiAsemanNimi(element.akt.timeTableRows[element.akt.timeTableRows.length - 1].stationUICCode);
+                console.log('Lähtö: ' + etsiAsemanNimi(element.akt.timeTableRows[0].stationUICCode));
+                console.log('Määränpää: ' + etsiAsemanNimi(element.akt.timeTableRows[element.akt.timeTableRows.length - 1].stationUICCode));
+            }
+        }
         // if (junanNro == element.trainNumber) {
         //     console.log('Lähtö: ' + etsiAsemanNimi(element.timeTableRows[0].stationUICCode));
         //     console.log('Määränpää: ' + etsiAsemanNimi(element.timeTableRows[element.timeTableRows.length - 1].stationUICCode));
