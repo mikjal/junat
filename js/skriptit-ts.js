66d0672107ea13714a6a79e2861a6088095de196
Tommi <tommi.savolainen79@gmail.com>
2024-01-22 11:48:53 +0200

    Funktioita rakennettu

--- js/skriptit-ts.js
+++ js/skriptit-ts.js
@@ -1,8 +1,14 @@
-function etsiLahtoJaMaaranpaa(junanNro) {
-    mt.junat.tiedot.forEach((element) => {
-        if (junanNro == element.trainNumber) {
-            console.log('Lähtö: ' + etsiAsemanNimi(element.timeTableRows[0].stationUICCode));
-            console.log('Määränpää: ' + etsiAsemanNimi(element.timeTableRows[element.timeTableRows.length - 1].stationUICCode));
-        }
+function etsiLahtoJaMaaranpaa(indeksi) {
+    junat.forEach((element, index) => {
+        console.log(element);
+        // if (junanNro == element.trainNumber) {
+        //     console.log('Lähtö: ' + etsiAsemanNimi(element.timeTableRows[0].stationUICCode));
+        //     console.log('Määränpää: ' + etsiAsemanNimi(element.timeTableRows[element.timeTableRows.length - 1].stationUICCode));
+        // }
+    });
+}
+function nimeaJuna(indeksi) {
+    junat.forEach((element, index) => {
+        console.log(element);
     });
 }
