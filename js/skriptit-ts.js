efbdc2eb70d6e995a0638ae2c45e7cf4d74582f1
Tommi <tommi.savolainen79@gmail.com>
2024-01-23 12:24:36 +0200

    tietojenHaku funktio muokkaa junien nimet

--- js/skriptit-ts.js
+++ js/skriptit-ts.js
@@ -1,12 +1,43 @@
 function tietojenHaku(indeksi) {
     let element = junat[indeksi];
     let operaattoriLista = mt.operaattorit.tiedot;
+    let junatyypit = {
+        HSM: 'Taajamajuna',
+        HDM: 'Taajamajuna',
+        IC: 'InterCity',
+        MUS: 'Museojuna',
+        MV: 'Kalustonsiirtojuna (kaukoliikenne)',
+        P: 'Pikajuna',
+        PYO: 'Yöpikajuna',
+        S: 'Pendolino',
+        AE: 'Allegro',
+        PVV: 'Pikajuna (Venäjä)',
+        HL: 'Lähijuna',
+        HV: 'Kalustonsiirtojuna (lähiliikenne)',
+        HLV: 'Veturivetoinen (lähiliikenne)',
+        PAI: 'Vaihtotyö',
+        T: 'Tavarajuna',
+        LIV: 'Radantarkastusvaunu',
+        MUV: 'Museojuna (vaihtotyö)',
+        PAI: 'Päivystäjä (veturi)',
+        SAA: 'Saatto',
+        TYO: 'Työjuna',
+        VET: 'Veturijuna',
+        VEV: 'Veturijuna',
+        VLI: 'Lisäveturi (vaihtotyö veturina)',
+    };
+    let nimiParit = Object.entries(junatyypit);
     if (element.akt != null) {
         element.tiedot.lahtopaikka = etsiAsemanNimi(element.akt.timeTableRows[0].stationUICCode);
         element.tiedot.maaranpaa = etsiAsemanNimi(element.akt.timeTableRows[element.akt.timeTableRows.length - 1].stationUICCode);
         operaattoriLista.forEach((operaattori) => {
             if (operaattori.operatorShortCode == element.akt.operatorShortCode) {
                 element.tiedot.operaattori = operaattori.operatorName;
+            }
+        });
+        nimiParit.map(([key, val] = entry) => {
+            if (element.akt.trainType == key) {
+                element.tiedot.nimi = val;
             }
         });
     }
