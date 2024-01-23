40990159a21c5369c0072e7fbf23c3d15d162c73
Tommi <tommi.savolainen79@gmail.com>
2024-01-23 11:00:24 +0200

    tietojenHaku funktio hakee operaattorin

--- js/skriptit-ts.js
+++ js/skriptit-ts.js
@@ -1,10 +1,13 @@
 function tietojenHaku(indeksi) {
     let element = junat[indeksi];
-    console.log(element);
+    let operaattoriLista = mt.operaattorit.tiedot;
     if (element.akt != null) {
         element.tiedot.lahtopaikka = etsiAsemanNimi(element.akt.timeTableRows[0].stationUICCode);
         element.tiedot.maaranpaa = etsiAsemanNimi(element.akt.timeTableRows[element.akt.timeTableRows.length - 1].stationUICCode);
-        console.log('Lähtö: ' + etsiAsemanNimi(element.akt.timeTableRows[0].stationUICCode));
-        console.log('Määränpää: ' + etsiAsemanNimi(element.akt.timeTableRows[element.akt.timeTableRows.length - 1].stationUICCode));
+        operaattoriLista.forEach((operaattori) => {
+            if (operaattori.operatorShortCode == element.akt.operatorShortCode) {
+                element.tiedot.operaattori = operaattori.operatorName;
+            }
+        });
     }
 }
