0023efe6ad6c2676e4f79f99c802a1a4da86bdfd
Tommi <tommi.savolainen79@gmail.com>
2024-01-23 09:54:45 +0200

    muutettu funktion nimi

--- js/skriptit-ts.js
+++ js/skriptit-ts.js
@@ -1,27 +1,10 @@
-function etsiLahtoJaMaaranpaa(indeksi) {
-    junat.forEach((element, index) => {
-        if (indeksi == index) {
-            console.log(element.akt);
-            if (element.akt != null) {
-                // if (element.tiedot == null) {
-                //     element.tiedot = '';
-                //     element.tiedot.lahtopaikka = ''
-                //     element.tiedot.maaranpaa = ''
-                // }
-                element.tiedot.lahtopaikka = etsiAsemanNimi(element.akt.timeTableRows[0].stationUICCode);
-                element.tiedot.maaranpaa = etsiAsemanNimi(element.akt.timeTableRows[element.akt.timeTableRows.length - 1].stationUICCode);
-                console.log('Lähtö: ' + etsiAsemanNimi(element.akt.timeTableRows[0].stationUICCode));
-                console.log('Määränpää: ' + etsiAsemanNimi(element.akt.timeTableRows[element.akt.timeTableRows.length - 1].stationUICCode));
-            }
-        }
-        // if (junanNro == element.trainNumber) {
-        //     console.log('Lähtö: ' + etsiAsemanNimi(element.timeTableRows[0].stationUICCode));
-        //     console.log('Määränpää: ' + etsiAsemanNimi(element.timeTableRows[element.timeTableRows.length - 1].stationUICCode));
-        // }
-    });
-}
-function nimeaJuna(indeksi) {
-    junat.forEach((element, index) => {
-        console.log(element);
-    });
+function tietojenHaku(indeksi) {
+    let element = junat[indeksi];
+    console.log(element);
+    if (element.akt != null) {
+        element.tiedot.lahtopaikka = etsiAsemanNimi(element.akt.timeTableRows[0].stationUICCode);
+        element.tiedot.maaranpaa = etsiAsemanNimi(element.akt.timeTableRows[element.akt.timeTableRows.length - 1].stationUICCode);
+        console.log('Lähtö: ' + etsiAsemanNimi(element.akt.timeTableRows[0].stationUICCode));
+        console.log('Määränpää: ' + etsiAsemanNimi(element.akt.timeTableRows[element.akt.timeTableRows.length - 1].stationUICCode));
+    }
 }
