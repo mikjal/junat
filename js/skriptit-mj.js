79ac3f39f83d9ff5f08b820fb124804cfcf49448
Mika Jalkanen <mika.jalkanen@kolumbus.fi>
2024-01-25 12:09:14 +0200

    Toinen kaavio + etuajassa/myöhässä tieto tooltippiin

--- js/skriptit-mj.js
+++ js/skriptit-mj.js
@@ -1,7 +1,8 @@
 let kartta,
     MQTTyhteys,
     debug = false,
-    junat = [];
+    junat = [],
+    paivitysjono = [];
 
 // metatiedot
 const mt = {
@@ -80,7 +81,6 @@ function piirraKarttamerkki(indeksi) {
         // merkki on jo olemassa, siirretään sitä
         juna.karttamerkki.setLatLng([juna.pkt.location.coordinates[1],juna.pkt.location.coordinates[0]]);
 
-
         if (juna.akt && juna.tiedot.nimi) {
             if (juna.karttamerkki._icon.classList.contains('harmaa')) {
                 juna.karttamerkki._icon.classList.remove('harmaa');
@@ -89,6 +89,9 @@ function piirraKarttamerkki(indeksi) {
             let tooltipTeksti = (juna.tiedot.nimi) ? '<strong>'+juna.tiedot.nimi+'</strong>' : juna.numero.toString();
             tooltipTeksti += (juna.tiedot.lahtopaikka && juna.tiedot.maaranpaa) ? '<br>' + juna.tiedot.lahtopaikka + ' - ' + juna.tiedot.maaranpaa : '';
             tooltipTeksti += (juna.tiedot.operaattori) ? '<br>' + juna.tiedot.operaattori : '';
+            if (juna.tiedot.aikaero != null) {
+                tooltipTeksti += (juna.tiedot.aikaero < 0) ? '<br>'+Math.abs(juna.tiedot.aikaero)+' minuuttia etuajassa' : (juna.tiedot.aikaero == 0) ? '<br>Aikataulussa' : '<br>'+juna.tiedot.aikaero+' minuuttia myöhässä';
+            }
             tooltipTeksti += (juna.tiedot.nopeus != null) ? '<br>Nopeus: ' + juna.tiedot.nopeus + ' km/h' : '';
             tooltipTeksti += (juna.pkt) ? '<br>Päivitetty '+(new Date(juna.pkt.timestamp)).toLocaleTimeString() : '';
 
@@ -113,11 +116,13 @@ function piirraKarttamerkki(indeksi) {
         }
     }
 
-    if (juna.akt == null && juna.tiedot.nimi == null) juna.karttamerkki._icon.classList.add('harmaa');
+    if (juna.akt == null && juna.tiedot.nimi == null) {
+        juna.karttamerkki._icon.classList.add('harmaa');
+    }
         
 }
 
-function paivitaJunanTiedot(onkoPaikkatieto, JSONtieto) {
+function paivitaJunanTiedot(JSONtieto) {
     // tarkistetaan että tiedoissa on junan numero
     if (JSONtieto.hasOwnProperty('trainNumber')) {
         // junan numero löytyy, selvitetään onko juna jo taulukossa
@@ -133,8 +138,8 @@ function paivitaJunanTiedot(onkoPaikkatieto, JSONtieto) {
         // luodaan viittaus junat-taulukossa olevaan juna-olioon
         let juna = junat[junanIndeksi];
 
-        // tuplavarmistus: onko kysessä paikkatieto?
-        if (onkoPaikkatieto && JSONtieto.hasOwnProperty('timestamp')) {
+        // onko kysessä paikkatieto (paikkatieto sisältää timestampin)?
+        if (JSONtieto.hasOwnProperty('timestamp')) {
             // käsitellään paikkatieto
             // tarkistetaan löytyykä vanha paikkatieto
             if (juna.pkt) { // juna.pkt != null
@@ -154,7 +159,7 @@ function paivitaJunanTiedot(onkoPaikkatieto, JSONtieto) {
             // piirretään karttamerkki
             piirraKarttamerkki(junanIndeksi);
 
-        }  // onkoPaikkatieto == false, varmistetaan että JSON-tiedoista löytyy version
+        }  // tarkistetaan onko tiedoissa version, jolloin kyseessä on muut junan tiedot
         else if (JSONtieto.hasOwnProperty('version')) {
             // käsitellään junan tiedot
             // tarkistetaan löytyykö vanha tieto
@@ -167,11 +172,14 @@ function paivitaJunanTiedot(onkoPaikkatieto, JSONtieto) {
                     return
                 }
                 juna.akt = JSONtieto;
+                aikatauluTarkistus(junanIndeksi);
+
             } else {
                 // vanhoja tietoja ei löydy
                 // kutsutaan funktiota joka luo junalle perustiedot
                 juna.akt = JSONtieto;
                 tietojenHaku(junanIndeksi);
+                aikatauluTarkistus(junanIndeksi);
             }
             
             // piirretään karttamerkki
@@ -225,6 +233,7 @@ function haeJSON(osoite, paluufunktio) {
 }
 
 
+/*
 function kasitteleMQTTJSON(kohdetieto,JSONtieto) {
 
     // tarkistetaan onko kyseessä paikkatieto, jos on onkoPaikkatieto=true,
@@ -236,6 +245,7 @@ function kasitteleMQTTJSON(kohdetieto,JSONtieto) {
     // jos onkoPaikkatieto on jotain muuta kuin null, päivitetään junan tiedot
     if (onkoPaikkatieto != null) paivitaJunanTiedot(onkoPaikkatieto, JSONtieto);
 }
+*/
 
 function asetaMQTTkuuntelija() {
     MQTTyhteys = new Paho.MQTT.Client('rata.digitraffic.fi', 443, 'myclientid_' + parseInt(Math.random() * 10000, 10));
@@ -247,7 +257,7 @@ function asetaMQTTkuuntelija() {
 
     // Mitä tehdään kun viesti saapuu:
     MQTTyhteys.onMessageArrived = function (message) {
-        kasitteleMQTTJSON(message.destinationName,JSON.parse(message.payloadString));
+        paivitaJunanTiedot(JSON.parse(message.payloadString));
     };
 
     let maaritykset = {
