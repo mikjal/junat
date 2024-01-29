548f4c38e9acc216df2200b803305b0f09c84dac
Tuomas Lappalainen <tuomas.lappalainen@esedulainen.fi>
2024-01-29 13:44:10 +0200

    Sivupaneelin alkeet

--- js/skriptit-tl.js
+++ js/skriptit-tl.js
@@ -1,54 +1,16 @@
-/* function piirraKarttamerkki(indeksi) {
-    
-  let juna = junat[indeksi];
 
-  if (juna.karttamerkki) {
-    juna.karttamerkki.setLatLng([juna.pkt.location.coordinates[1], juna.pkt.location.coordinates[0]]);
+function sivuPaneeli() {
+  var sidebar = L.control.sidebar('sidebar', {
+    position: 'left'
+  });
 
-    if (juna.akt && juna.tiedot.nimi) {
-      if (juna.karttamerkki._icon.classList.contains('harmaa')) {
-        juna.karttamerkki._icon.classList.remove('harmaa');
-      }
-
-      let tooltipTeksti = (juna.tiedot.nimi) ? '<strong>' + juna.tiedot.nimi + '</strong>' : juna.numero.toString();
-      tooltipTeksti += (juna.tiedot.lahtopaikka && juna.tiedot.maaranpaa) ? '<br>' + juna.tiedot.lahtopaikka + ' - ' + juna.tiedot.maaranpaa : '';
-      tooltipTeksti += (juna.tiedot.operaattori) ? '<br>' + juna.tiedot.operaattori : '';
-      if (juna.tiedot.aikaero != null) {
-        tooltipTeksti += (juna.tiedot.aikaero < 0) ? '<br>' + Math.abs(juna.tiedot.aikaero) + ' minuuttia etuajassa' : (juna.tiedot.aikaero == 0) ? '<br>Aikataulussa' : '<br>' + juna.tiedot.aikaero + ' minuuttia myöhässä';
-      }
-      tooltipTeksti += (juna.tiedot.nopeus != null) ? '<br>Nopeus: ' + juna.tiedot.nopeus + ' km/h' : '';
-      tooltipTeksti += (juna.pkt) ? '<br>Päivitetty ' + (new Date(juna.pkt.timestamp)).toLocaleTimeString() : '';
-
-      juna.karttamerkki.setTooltipContent(tooltipTeksti);
-    }
-
-  } else {
-    if (juna.pkt) {
-      let uusiKarttamerkki = L.marker([juna.pkt.location.coordinates[1], juna.pkt.location.coordinates[0]])
-        .addTo(kartta);
-
-      uusiKarttamerkki.on('click', function () {
-        console.log('Klikattiin junan', juna.numero, 'merkkiä.');
-        let popupTeksti = '<strong>' + juna.tiedot.nimi + '</strong>' +
-          '<br>' + juna.tiedot.lahtopaikka + ' - ' + juna.tiedot.maaranpaa +
-          '<br>' + juna.tiedot.operaattori +
-          '<br>' + (juna.tiedot.aikaero < 0 ? Math.abs(juna.tiedot.aikaero) + ' minuuttia etuajassa' :
-            (juna.tiedot.aikaero == 0 ? 'Aikataulussa' : juna.tiedot.aikaero + ' minuuttia myöhässä')) +
-          '<br>Nopeus: ' + juna.tiedot.nopeus + ' km/h' +
-          '<br>Päivitetty ' + (new Date(juna.pkt.timestamp)).toLocaleTimeString();
-
-        juna.karttamerkki.unbindPopup().bindPopup(popupTeksti).togglePopup();
-      });
-
-      juna.karttamerkki = uusiKarttamerkki;
-    }
-  }
-
-  if (juna.akt == null && juna.tiedot.nimi == null) {
-    juna.karttamerkki._icon.classList.add('harmaa');
-  }
-} */
-
+  kartta.addControl(sidebar);
+  let uusiKarttamerkki = L.marker([])
+  uusiKarttamerkki.on('click', function () {
+    sidebar.toggle();
+    document.getElementById('siderbar-content').innerHTML = '<h2>Test</h2>';
+  });
+}
 const aikaElement = document.querySelector(".aika");
 const pvmElement = document.querySelector(".paivam");
 
@@ -79,24 +41,24 @@ function formatDate(pvm) {
     "Lauantai"
   ];
   const Kuukaudet = [
-    "Tammikkuu",
-    "Helmikuu",
-    "Maaliskuu",
-    "Huhtikuu",
-    "Toukokuu",
-    "Kesäkuu",
-    "Heinäkuu",
-    "Elokuu",
-    "Syyskuu",
-    "Lokakuu",
-    "Marraskuu",
-    "Joulukuu"
+    "Tammikkuuta",
+    "Helmikuuta",
+    "Maaliskuuta",
+    "Huhtikuuta",
+    "Toukokuuta",
+    "Kesäkuuta",
+    "Heinäkuuta",
+    "Elokuuta",
+    "Syyskuuta",
+    "Lokakuuta",
+    "Marraskuuta",
+    "Joulukuuta"
   ];
 
-  return `${Paivat[pvm.getDay()]}, ${
-    Kuukaudet[pvm.getMonth()]
-  } ${pvm.getDate()} ${pvm.getFullYear()}`;
+  return `${Paivat[pvm.getDay()]}. ${pvm.getDate()} ${Kuukaudet[pvm.getMonth()]
+    }  ${pvm.getFullYear()}`;
 }
+
 
 setInterval(() => {
   const now = new Date();
