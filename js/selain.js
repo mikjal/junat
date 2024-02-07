16b75ae70adc2c9340d1e16faff2abdbf33375d3
Tuomas Lappalainen <tuomas.lappalainen@esedulainen.fi>
2024-02-08 01:58:08 +0200

    Selain.js päivitystä

--- js/selain.js
+++ js/selain.js
@@ -1,51 +1,68 @@
 // Selaimen tarkastamiseen liittyvä koodi tänne
 
-	var objappVersion = navigator.appVersion; 
-	var selainAgent = navigator.userAgent; 
-	var selainNimi = navigator.appName; 
-	var selainVersio = '' + parseFloat(navigator.appVersion); 
-	var Offset, OffsetVersion, ix; 
-	
+var objappVersio = navigator.appVersion;
+var selainAgent = navigator.userAgent;
+var selainNimi = navigator.appName;
+var selainVersio = '' + parseFloat(navigator.appVersion);
+var Offset, OffsetVersion, ix;
+
+if (typeof navigator !== 'undefined' && 'onLine' in navigator) {
+
 	// Chrome 
-	if ((OffsetVersion = selainAgent.indexOf("Chrome")) != -1) { 
-		selainNimi = "Chrome"; 
-		selainVersio = selainAgent.substring(OffsetVersion + 7); 
-	} 
-	
+	if ((OffsetVersion = selainAgent.indexOf("Chrome")) != -1) {
+		selainNimi = "Chrome";
+		selainVersio = selainAgent.substring(OffsetVersion + 7);
+
+		if(parseFloat(selainVersio) < 41) {
+			window.stop();
+		}
+	}
+
+	// Opera
+
+	if((OffsetVersion = selainAgent.indexOf("Opera")) != -1) {
+		selainNimi = "Opera";
+		selainVersio = selainAgent.substring(OffsetVersion + 7);
+		if (parseFloat(selainVersio) < 17)
+		window.stop();
+	}
+
 	// Microsoft internet explorer 
-	else if ((OffsetVersion = selainAgent.indexOf("MSIE")) != -1) { 
-		window.stop()
-    
-	} 
-	
+	else if ((OffsetVersion = selainAgent.indexOf("MSIE")) != -1) {
+			window.stop();
+	}
+
 	// Firefox 
-	else if ((OffsetVersion = selainAgent.indexOf("Firefox")) != -1) { 
-		selainNimi = "Firefox"; 
-	} 
-	
+	else if ((OffsetVersion = selainAgent.indexOf("Firefox")) != -1) {
+		selainNimi = "Firefox";
+		selainVersio = selainAgent.substring(OffsetVersion + 7);
+		if(parseFloat(selainVersio) < 44) {
+			window.stop();
+		}
+	}
+
 	// Safari 
-	else if ((OffsetVersion = selainAgent.indexOf("Safari")) != -1) { 
-		selainNimi = "Safari"; 
-		selainVersio = selainAgent.substring(OffsetVersion + 7); 
-		if ((OffsetVersion = selainAgent.indexOf("Version")) != -1) 
-			selainVersio = selainAgent.substring(OffsetVersion + 8); 
-	} 
+	else if ((OffsetVersion = selainAgent.indexOf("Safari")) != -1) {
+		selainNimi = "Safari";
+		selainVersio = selainAgent.substring(OffsetVersion + 7);
+		if ((OffsetVersion = selainAgent.indexOf("Version")) != -1)
+			selainVersio = selainAgent.substring(OffsetVersion + 8);
+		if(parseFloat(selainVersio) < 10) {
+			window.stop();
+		}
+	}
+
+	// Edge
+	else if ((OffsetVersion = selainAgent.indexOf("Edge")) != -1) {
+		selainNimi = "Edge";
+		selainVersio = selainAgent.substring(OffsetVersion + 7);
+	}
+	if ((ix = selainVersio.indexOf(";")) != -1)
+		selainVersio = selainVersio.substring(0, ix);
+	if ((ix = selainVersio.indexOf(" ")) != -1)
+		selainVersio = selainVersio.substring(0, ix);
+
 	
-	// Muille selaimille
-	else if ((Offset = selainAgent.lastIndexOf(' ') + 1) < 
-		(OffsetVersion = selainAgent.lastIndexOf('/'))) { 
-		selainNimi = selainAgent.substring(Offset, OffsetVersion); 
-		selainVersio = selainAgent.substring(OffsetVersion + 1); 
-		if (selainNimi.toLowerCase() == selainNimi.toUpperCase()) { 
-			selainNimi = navigator.appName; 
-		} 
-	} 
-	
-	if ((ix = selainVersio.indexOf(";")) != -1) 
-		selainVersio = selainVersio.substring(0, ix); 
-	if ((ix = selainVersio.indexOf(" ")) != -1) 
-		selainVersio = selainVersio.substring(0, ix); 
-	
-	
-	
+}
+
 
